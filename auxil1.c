1b00053058ec68705bcafc9a343c2408746c2adb
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-03-17 21:43:10 -0500

    test

--- auxil1.c
+++ auxil1.c
@@ -236,7 +236,6 @@ int MyKmeans_p(const float *inputDataIn, int *clustIdRet, int *counterRet, const
             assert(dist2Min > 90000.0F);
             assert(clusterCount > 0);
             for (int clusterOn = 0; clusterOn < clusterCount; ++clusterOn) {
-                printf("cluster %d", clusterOn);
 
                 int clusterStartIdx = featureCount * clusterOn;
                 double dist2 = 0;
@@ -255,7 +254,8 @@ int MyKmeans_p(const float *inputDataIn, int *clustIdRet, int *counterRet, const
                     float expect = centers[clusterIdx];
                     double difference = on - expect;
 
-                    printf("difference %f\n", difference);
+                    printf("cluster %d, difference %f, on %f, expect %f\n", clusterOn, difference, (double) on,
+                           (double) expect);
                     double d2 = difference * difference;
                     assert__(d2 < 100000.0 * 100000.0) {
                         printf("assert failed... on [%f], expeect[%f]\n", on, expect);
